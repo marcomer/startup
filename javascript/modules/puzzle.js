4a41823c90ef7311a721ba81471ebad16a307329
Marcus Omer <marcus.omer19@gmail.com>
2023-12-07 23:33:10 -0700

    Create sudoku puzzle generator

--- javascript/modules/puzzle.js
+++ javascript/modules/puzzle.js
@@ -1,10 +1,33 @@
-// contains the puzzle class and ways to generate and get puzzles
+// module containing the Puzzle class and ways to generate and get puzzles
+
+// global timer
+
+class Value {
+  value;
+  editable;
+
+  /**
+   * Construct a new Value object.
+   * @param {number} value a number from 1-9
+   * @param {boolean} editable whether or not the value can be edited/changed
+   */
+  constructor(value, editable = true) {
+    this.value = value;
+    this.editable = editable;
+  }
+}
 
 export class Puzzle {
   #solution;
   #table;
   #uuid;
 
+  /**
+   * Construct a new Puzzle object.
+   * @param {Array<Array<number>>} solution 2D array (9x9) of numbers (1-9)
+   * @param {Array<Array<Value>>} table 2D array (9x9) of Value objects
+   * @param {string} uuid a unique identifer for the Puzzle
+   */
   constructor(solution, table, uuid) {
     //this.#solution = JSON.parse(JSON.stringify(solution));
     //this.#table = JSON.parse(JSON.stringify(table));
@@ -13,29 +36,63 @@ export class Puzzle {
     this.#uuid = uuid;
   }
 
+  /**
+   * Get the uuid associated with the puzzle.
+   * @returns {string} uuid
+   */
   get getUUID() {
     return this.#uuid;
   }
 
+  /**
+   * Get a value from the table (not the solution).
+   * @param {number} row the table row
+   * @param {number} col the table column
+   * @returns {number} the value at table[`row`][`col`], or 0 if `row` or `col` are out-of-range
+   */
   getValueAt(row, col) {
     if (row < 0 || col < 0 || row > 8 || col > 8) {
       return 0;
     }
-    return this.#table[row][col];
+    return this.#table[row][col].value;
   }
 
+  /**
+   * Attempts to insert a value in the table at [`row`][`col`]. If the `value`
+   * violates any constraints then false will be returned along with an array
+   * of table positions that the `value` violates. The `value` will not be inserted
+   * unless it does not violate any constraints.
+   * 
+   * @param {number} row the table row
+   * @param {number} col the table column
+   * @param {number} value the value to be inserted
+   * @returns {[boolean, [[number,number]]]} a boolean success var and an 
+   * array of table positions that the value violates
+   */
   insertValueAt(row, col, value) {
     if (value > 9 || value < 1 || row < 0 || col < 0 || row > 8 || col > 8 ) {
       return [false, []];
     }
     const [passed, violations] = this.checkConstraints(row, col, value);
     if (passed) {
-      this.#table[row][col] = value;
+      this.#table[row][col].value = value;
       return [true, violations];
     }
     return [false, violations];
   }
 
+  /**
+   * Checks a given `value` in the table at [`row`][`col`] against the puzzle constraints. 
+   * If the `value` violates any constraints then false will be returned along with an array
+   * of table positions that the `value` violates. Does not attempt to insert the value, only
+   * checks it.
+   * 
+   * @param {number} row the table row
+   * @param {number} col the table column
+   * @param {number} value the value to be checked
+   * @returns {[boolean, [[number,number]]]} a boolean success var and an 
+   * array of table positions that the value violates
+   */
   checkConstraints(row, col, value) {
     if (value > 9 || value < 1 || row < 0 || col < 0 || row > 8 || col > 8 ) {
       return [false, []];
@@ -48,17 +105,17 @@ export class Puzzle {
     
     // check against col and row
     for (let i = 0; i < 9; i++) {
-      if (this.#table[i][col] === value) {
+      if (this.#table[i][col].value === value) {
         violations.push([i, col]);
       }
-      if (this.#table[row][i] === value) {
+      if (this.#table[row][i].value === value) {
         violations.push([row, i]);
       }
     }
 
     // check against square unit
-    const rmin = row;
-    const cmin = col;
+    let rmin = row;
+    let cmin = col;
     while (rmin % 3 !== 0) {
       rmin--;
     }
@@ -67,7 +124,7 @@ export class Puzzle {
     }
     for(let r = rmin; r < rmin + 3; r++) {
       for (let c = cmin; c < cmin + 3; c++) {
-        if (this.#table[r][c] === value) {
+        if (this.#table[r][c].value === value) {
           const array = [r, c];
           if (violations.length === 0 || !violations.some(a => array.every((v, i) => v === a[i]))) {
             // violations does not contain the array
@@ -83,31 +140,324 @@ export class Puzzle {
     return [false, violations];
   }
 
+  /**
+   * Determines if the puzzle has been solved. Compares the table with the solution.
+   * @returns {boolean} true if the puzzle is solved, false if the puzzle is not yet solved
+   */
+  solved() {
+    for (let r = 0; r < 9; r++) {
+      for (let c = 0; c < 9; c++) {
+        if (this.#solution[r][c] !== this.#table[r][c].value) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  editable(row, col) {
+    if (row < 0 || col < 0 || row > 8 || col > 8) {
+      return false;
+    }
+    return this.#table[row][col].editable;
+  }
+
 
   
 }
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function squareContainsValue(row, col, value, table) {
+  for (let r = row; r < row + 3; r++) {
+    for (let c = col; c < col + 3; c++) {
+      if (table[r][c] === value) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function rowContainsValue(row, value, table) {
+  for (let c = 0; c < 9; c++) {
+    if (table[row][c] === value) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function colContainsValue(col, value, table) {
+  for (let r = 0; r < 9; r++) {
+    if (table[r][col] === value) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function shuffleArray(arr) {
+  return arr.sort(() => Math.random() - 0.5);
+}
+
+
+
+function isSafe(row, col, value, table) {
+  let rmin = row;
+  let cmin = col;
+  while (rmin % 3 !== 0) {
+    rmin--;
+  }
+  while (cmin % 3 !== 0) {
+    cmin--;
+  }
+  return !squareContainsValue(rmin, cmin, value, table) 
+      && !rowContainsValue(row, value, table) 
+      && !colContainsValue(col, value, table)
+}
+
+function verifyBoard(table) {
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      const backup = table[r][c];
+      table[r][c] = 0; 
+      if (!isSafe(r, c, backup, table)) {
+        table[r][c] = backup;
+        return false;
+      }
+      table[r][c] = backup;
+    }
+  }
+  return true;
+}
+
+function getNextVal(r, c, table) {
+  c++;
+  if (c > 8) {
+    c = 0;
+    r++;
+  }
+  if (r > 8) {
+    return -1;
+  }
+  return table[r][c];
+}
+
+function fillPuzzle(r, c, table) {
+  if (c > 8) {
+    c = 0;
+    r++;
+  }
+
+  if (r === 8 && c === 8) {
+    if (table[r][c] > 0) {
+      return;
+    }
+    // last item
+    for (let i = 1; i < 10; i++) {
+      if (isSafe(r, c, i, table)) {
+        table[r][c] = i;
+        return; // success, return
+      }
+    }
+    table[r][c] = 0;
+    return; // failed, return
+  } 
+
+  if (table[r][c] !== 0) {
+    fillPuzzle(r, c + 1, table);
+    return;
+  }
+
+  // any item except last
+  let tried = [];
+  while (getNextVal(r, c, table) === 0) {
+    // find a new num that hasn't been tried
+    while (tried.length < 9) {
+      let newTry = getRandomInt(1, 9);
+      if (tried.includes(newTry)) {
+        continue;
+      }
+      tried.push(newTry);
+      if (isSafe(r, c, newTry, table)) {
+        table[r][c] = newTry;
+        break;
+      }
+    }
+    if (table[r][c] === 0 || tried.length === 9) {
+      table[r][c] = 0;
+      return; // went through all tries, reset
+    }
+    fillPuzzle(r, c + 1, table);
+    if (getNextVal(r, c, table) !== 0) {
+      return; // success
+    }
+  }
+  return;
+}
+
+function getEmptyBoxes(table) {
+  let boxes = [];
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      if (table[r][c] === 0) {
+        boxes.push([r,c]);
+      }
+    }
+  }
+  return boxes;
+}
+
+class PuzzleSolver {
+  table;
+
+  constructor(table) {
+    this.table = table;
+  }
+
+  hasUniqueSolution() {
+    let solutions = this.solve(false);
+    return solutions.length;
+  }
+
+  solve(findAll = true) {
+    let solutions = [];
+    let copy = JSON.parse(JSON.stringify(this.table));
+    try {
+      this.#rsolve(copy, solutions, findAll);
+    } catch(error) {
+      // found more than two solutions (do not find all)
+      console.log("More than 1 solution found");
+    }
+    return solutions;
+  }
+
+  #rsolve(table, solutions, findAll) {
+    for (let r = 0; r < 9; r++) {
+      for (let c = 0; c < 9; c++) {
+        if (table[r][c] === 0) {
+          // try possible values
+          for (let i = 1; i < 10; i++) {
+            if (isSafe(r, c, i, table)) {
+              table[r][c] = i;
+              this.#rsolve(table, solutions, findAll);
+              if (!findAll && solutions.length > 1) {
+                throw Error ("Solve timeout");
+              }
+              table[r][c] = 0;
+            }
+          }
+          return;
+        }
+      }
+    }
+    if (getEmptyBoxes(table).length === 0) {
+      solutions.push(JSON.parse(JSON.stringify(table)));
+    }
+    return;
+  }
+}
+
+function removeBoxes(n, table, actual) {
+  let count = n;
+  let coords = [];
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      let arr = [r,c];
+      coords.push(arr);
+    }
+  }
+  shuffleArray(coords);
+  let solver = new PuzzleSolver(table);
+
+  while (count > 0 && coords.length > 0) {
+    const [r,c] = coords.pop();
+    const backup = table[r][c];
+    table[r][c] = 0;
+    if (solver.hasUniqueSolution() === 1) {
+      count--;
+    } else {
+      table[r][c] = backup;
+    }
+  }
+}
+
+/**
+ * Helper function for generatePuzzle(). Creates a puzzle with a unique solution.
+ */
+function generatePuzzleHelper() {
+  try {
+    // generate the complete puzzle
+    let solution = [[0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]];
+
+    fillPuzzle(0, 0, solution);
+    console.log("Solution:");
+    console.log(solution);
+    console.log("Verifying constraints on solution...");
+    console.log(verifyBoard(solution));
+    if (!verifyBoard(solution)) {
+      throw Error ("Solution did not fit constraints");
+    }
+
+    // create the partial version of the solution
+    let partial = JSON.parse(JSON.stringify(solution));
+    console.log("Removing boxes...");
+    removeBoxes(50, partial, solution);
+    console.log("Partial board:");
+    console.log(partial);
+    console.log("Verifying that there is one, unique solution...");
+    let solver = new PuzzleSolver(partial);
+    console.log(solver.hasUniqueSolution() === 1);
+    if (solver.hasUniqueSolution() !== 1) {
+      throw Error ("Does not have unique solution");
+    }
+
+    console.log(`Removed ${getEmptyBoxes(partial).length} boxes`);
+    if (getEmptyBoxes(partial).length !== 50) {
+      throw Error ("Did not remove enough boxes");
+    }
+
+    let s = partial.join();
+    s = s.replaceAll(",", "");
+    s = s.replaceAll("0", ".");
+    console.log(s);
+    //turn everything in partial to a value
+    for (let r = 0; r < 9; r++) {
+      for (let c = 0; c < 9; c++) {
+        if (partial[r][c] === 0) {
+          partial[r][c] = new Value(partial[r][c], true);
+        } else {
+          partial[r][c] = new Value(partial[r][c], false);
+        }
+      }
+    }
+
+    return new Puzzle(solution, partial, crypto.randomUUID());
+  } catch (error) {
+    console.log(error.message);
+    return generatePuzzleHelper();
+  }
+}
+
 
 /**
  * Generate a new puzzle with a unique solution.
  */
 export async function generatePuzzle() {
-  // generate the complete puzzle
-  let solution = [[0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0]];
-
-  // TODO:
-
-  // generate the partially complete puzzle
-  let partial = JSON.parse(JSON.stringify(solution));
-
-  return new Puzzle(solution, partial, crypto.randomUUID());
+  return generatePuzzleHelper();
 }
 
 
--- javascript/solve.js
+++ javascript/solve.js
@@ -1,2 +1,127 @@
 import {Puzzle, generatePuzzle, getPuzzle} from "./modules/puzzle.js"
 
+class Game {
+  #puzzle;
+
+  /**
+   * Construct a new Game object.
+   * @param {Puzzle} puzzle a Puzzle
+   */
+  constructor(puzzle) {
+    this.#puzzle = puzzle;
+  }
+
+
+  displayViolations(row, col, violations) {
+    // reset any background colors
+
+    // insert all numbers
+  }
+
+  resetDisplay() {
+    // reset any background colors
+    
+    // insert all numbers
+  }
+
+  displaySolvedAnimation() {
+
+  }
+
+  /**
+   * Insert a value into the puzzle. If there are violations, the value is not
+   * inserted and the violations are displayed. If the value is inserted, a check
+   * is made to see if the puzzle is solved.
+   * @param {number} row the table row
+   * @param {number} col the table column
+   * @param {number} value the value to be inserted
+   * @return {void}
+   */
+  insertValue(row, col, value) {
+    const [success, violations] = this.#puzzle.insertValueAt(row, col, value);
+    if (success) {
+      // TODO: save the puzzle in the database
+
+      if (this.#puzzle.solved()) {
+        // puzzle has been solved
+        this.displaySolvedAnimation();
+
+        // increase solved counter
+        
+      }
+    }
+    else {
+      if (violations.length > 0) {
+        this.displayViolations(row, col, violations);
+      }
+    }
+  }
+
+
+  getValueAsString(row, col) {
+    const val = this.#puzzle.getValueAt(row, col).toString();
+    if (val === "0") {
+      return "";
+    }
+    return val;
+  }
+
+  editable(row, col) {
+    return this.#puzzle.editable(row, col);
+  }
+
+
+
+}
+
+
+const isNum = new RegExp("[1-9]{1}");
+
+function parseRowAndCol(id) {
+  const match = id.match(isNum);
+  return [parseInt(match[0]), parseInt(match[1])];
+}
+
+
+// create the game
+// get the puzzle
+let game = new Game(getPuzzle());
+
+// INITIALIZE PAGE
+
+let tdElements = document.getElementsByTagName("td");
+for (let td of tdElements) {
+  let input = td.firstElementChild;
+  if (input !== null) {
+    const [row, col] = parseRowAndCol(input.id);    // get row, col
+    input.value = game.getValueAsString(row, col);  // set input's value to puzzle's value
+
+    if (game.editable(row, col)) {
+      // input box can be edited
+
+      // create input event listener that checks and inserts value into puzzle
+      input.addEventListener("input", function(event) {
+        if (event.target.value.length === 0) {
+          return;
+        }
+        const [row, col] = parseRowAndCol(event.target.id);
+        const puzzleVal = game.getValueAsString(row, col);
+        const value = event.target.value[event.target.value.length - 1];
+        game.resetDisplay();
+  
+        event.target.value = "";  //reset input value
+        if (isNum.test(value)) {
+          // is a single number from 1-9, insert the value
+          event.target.value = value; 
+          game.insertValue(row, col, parseInt(value));
+        }
+      });
+    }
+    else {
+      // input box cannot be edited
+      input.setAttribute("disabled", "disabled"); // disable editing on box
+    }
+  }
+}
+
+
