4ae3e8465bd6aa383a4baabdd0094beee179c486
Marcus Omer <marcus.omer19@gmail.com>
2024-03-16 00:35:02 -0600

    Refactor history page to use api
    The history page now uses the puzzle api defined in index.js.
    When loaded, history.js will request the most recent puzzles
    and then dynamically create elements to display them.
    Consider adding a loading screen since generating 12 puzzles can take
    time.

--- service/public/history.js
+++ service/public/history.js
@@ -1,36 +1,146 @@
-import {Puzzle, generatePuzzle, getPuzzle} from "./modules/puzzle.js";
+import { Puzzle } from "./modules/puzzle.js";
+
+let uuids = [];
 
 function getValueAsString(row, col, puzzle) {
   const val = puzzle.getValueAt(row, col).toString();
   if (val === "0") {
-    return " ";
+    return "&nbsp;";
   }
   return val;
 }
 
-/**
- * Return 12 of the most recently worked on puzzles from the user's database profile.
- */
-async function mostRecentPuzzles() {
-  //TODO: in the future this should get a list of uuids/puzzles? from the database
-
-  // for each puzzle, 
-  for (let p = 1; p < 13; p++) {
-    let table = document.getElementById(`puzzle${p}`);
-    if (table === null) {
-      continue;
+function createTableElement(puzzle, puzzleIndex) {
+  const table = document.createElement("table");
+  table.id = `puzzle${puzzleIndex}`;
+  for (let row = 0; row < 9; row++) {
+    const tr = document.createElement("tr");
+    for (let col = 0; col < 9; col++) {
+      const td = document.createElement("td");
+      td.innerHTML = getValueAsString(row, col, puzzle);
+      tr.appendChild(td);
     }
-    let puzzle = await generatePuzzle(50);
-    let tbody = table.children.item(0);
-    for (let r = 0; r < 9; r++) {
-      let tr = tbody.children.item(r);
-      for (let c = 0; c < 9; c++) {
-        let td = tr.children.item(c);
-        td.textContent = getValueAsString(r, c, puzzle);
-      }
-    }
+    table.appendChild(tr);
   }
-
+  return table;
 }
 
-await mostRecentPuzzles();
+function createMenuElement(puzzle, className) {
+  // create menu
+  const menu = document.createElement("div");
+  menu.className = className;
+
+  // append generated date
+  const genDateEl = document.createElement("p");
+  const genDate = puzzle.getDateGenerated();
+  genDateEl.textContent = `Generated ${genDate.getUTCMonth() + 1}/${genDate.getUTCDate()}/${genDate.getUTCFullYear()}`;
+  menu.appendChild(genDateEl);
+
+  if (className === "solved-menu") {
+    // append solve date
+    const solveDateEl = document.createElement("p");
+    const solveDate = puzzle.getDateSolved();
+    solveDateEl.textContent = `Solved ${solveDate.getUTCMonth() + 1}/${solveDate.getUTCDate()}/${solveDate.getUTCFullYear()}`;
+    menu.appendChild(solveDateEl);
+  }
+
+  if (className === "unsolved-menu") {
+    // append continue button form
+    const form = document.createElement("form");
+    form.setAttribute("method", "get");
+    //TODO: change solve api form.setAttribute("action", `/solve/${puzzle.getUUID()}`)
+    form.setAttribute("action", "/solve");
+
+    const button = document.createElement("button");
+    button.setAttribute("type", "submit");
+    button.classList.add("btn", "continue-btn");
+    button.textContent = "Continue";
+
+    form.appendChild(button);
+    menu.appendChild(form);
+  }
+
+  return menu;
+}
+
+/**
+ * Creates and returns a "unsolved-puzzle" element.
+ */
+function createUnsolvedPuzzleElement(puzzle, puzzleIndex) {
+  const parent = document.createElement("div");
+  parent.className = "unsolved-puzzle";
+  
+  // append table
+  const table = createTableElement(puzzle, puzzleIndex);
+  parent.appendChild(table);
+
+  // append menu
+  const menu = createMenuElement(puzzle, "unsolved-menu");
+  parent.appendChild(menu);
+
+  return parent;
+}
+
+/**
+ * Creates and returns a "solved-puzzle" element.
+ */
+function createSolvedPuzzleElement(puzzle, puzzleIndex) {
+  const parent = document.createElement("div");
+  parent.className = "solved-puzzle";
+  
+  // append table
+  const table = createTableElement(puzzle, puzzleIndex);
+  parent.appendChild(table);
+
+  // append menu
+  const menu = createMenuElement(puzzle, "unsolved-menu");
+  parent.appendChild(menu);
+
+  return parent;
+}
+
+async function getMostRecentPuzzles() {
+  const user = localStorage.getItem("username");
+  let response = await fetch(`/api/puzzle/${user}/recent`);
+  uuids = await response.json();
+
+  const puzzles = [];
+  for (let i = 0 ; i < uuids.length; i++) {
+    //get puzzle
+    response = await fetch(`/api/puzzle/${user}/${uuids[i]}`);
+    const {solution, table, uuid, genDate, solveDate } = await response.json();
+    puzzles[i] = new Puzzle(solution, table, uuid, genDate, solveDate);
+  }
+  //TODO: should it sort by date and if solved? or should api do that?
+  return puzzles;
+}
+
+
+/**
+ * Display the user's most recent puzzles on the page.
+ */
+async function displayPuzzles() {
+
+  try {
+    const puzzles = await getMostRecentPuzzles(); 
+
+    const grid = document.getElementsByClassName("puzzle-grid")[0];
+
+    // display puzzles
+    for (let i = 0; i < puzzles.length; i++) {
+      let puzzleEl;
+      if (puzzles[i].solved()) {
+        puzzleEl = createSolvedPuzzleElement(puzzles[i], i);
+      } else {
+        puzzleEl = createUnsolvedPuzzleElement(puzzles[i], i);
+      }
+
+      grid.appendChild(puzzleEl);
+      uuids.push(puzzles[i].getUUID());
+    }
+  } catch(error) {
+    console.log(`An error occurred: ${error.message}`);
+  }
+}
+
+await displayPuzzles();
