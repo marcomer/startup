484b4b141ad0ffdeff0e58c1af764b96f649dc5b
Marcus Omer <marcus.omer19@gmail.com>
2024-03-19 18:46:32 -0600

    Refactor to call stats api

--- service/public/statistics.js
+++ service/public/statistics.js
@@ -1,17 +1,18 @@
-import {Database} from "./modules/db.js";
 
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
 
-function updateScoreboard() {
-  // generate a scoreboard
-  let tbody = document.getElementsByTagName("tbody")[0];
-  let scores = [];
-  for (let i = 0; i < 10; i++) {
-    scores.push(Math.floor(Math.random() * 100));
+async function updateScoreboard() {
+  const user = localStorage.getItem("username");
+  if (user === null) {
+    throw Error("LocalStorage is missing \"username\" item");
   }
-  scores.sort((a,b) => b - a);
 
+  const response = await fetch(`/api/stats/scoreboard`);
+  const { scores } = await response.json();
+  console.log(scores);
+
+  let tbody = document.getElementsByTagName("tbody")[0];
   for (let i = 0; i < 10; i++) {
     let tr = tbody.children.item(i);
     let td = tr.children.item(1);
@@ -21,69 +22,60 @@ function updateScoreboard() {
 
 function updateGlobalGenerated(n) {
   let span = document.getElementById("globalGenerated");
-  if (Database.getGlobalGenerated() === null) {
-    Database.setGlobalGenerated(1000);
-  }
-  Database.setGlobalGenerated(Database.getGlobalGenerated() + n);
-  span.innerHTML = `${Database.getGlobalGenerated()}`;
+  span.innerHTML = `${n}`;
 }
 
 function updateGlobalSolved(n) {
   let span = document.getElementById("globalSolved");
-  if (Database.getGlobalSolved() === null) {
-    Database.setGlobalSolved(500);
-  }
-  Database.setGlobalSolved(Database.getGlobalSolved() + n);
-  span.innerHTML = `${Database.getGlobalSolved()}`;
+  span.innerHTML = `${n}`;
 }
 
 function updateTotalPlayers(n) {
   let span = document.getElementById("totalPlayers");
-  if (Database.getTotalPlayers() === null) {
-    Database.setTotalPlayers(700);
-  }
-  Database.setTotalPlayers(Database.getTotalPlayers() + n);
-  span.innerHTML = `${Database.getTotalPlayers() + n}`;
+  span.innerHTML = `${n}`;
 }
 
 function updateUserGenerated(n) {
   let span = document.getElementById("userGenerated");
-  if (Database.getUserGenerated() === null) {
-    Database.setUserGenerated(50);
-  }
-  Database.setUserGenerated(Database.getUserGenerated() + n);
-  span.innerHTML = `${Database.getUserGenerated() + n}`;
+  span.innerHTML = `${n}`;
 }
 
 function updateUserSolved(n) {
   let span = document.getElementById("userSolved");
-  if (Database.getUserSolved() === null) {
-    Database.setUserSolved(25);
-  }
-  Database.setUserSolved(Database.getUserSolved() + n);
-  span.innerHTML = `${Database.getUserSolved() + n}`;
+  span.innerHTML = `${n}`;
 }
 
 function setUserScoreboard(n) {
-  let span = document.getElementById("globalGenerated");
-  if (Database.getUserScoreboard() === null) {
-    Database.setUserScoreboard(100);
+  let span = document.getElementById("userScoreboard");
+  span.innerHTML = `#${n}`;
+}
+
+
+async function update() {
+  const user = localStorage.getItem("username");
+  if (user === null) {
+    throw Error("LocalStorage is missing \"username\" item");
   }
-  Database.setUserScoreboard(n);
-  span.innerHTML = `${Database.getUserScoreboard()}`;
+
+
+  // get stats
+  const response = await fetch(`/api/stats/${user}`);
+  const { globalStats, userStats } = await response.json();
+  console.log(globalStats);
+  console.log(userStats);
+  
+  updateUserGenerated(userStats.puzzlesGenerated);
+  updateUserSolved(userStats.puzzlesSolved);
+  updateGlobalGenerated(globalStats.puzzlesGenerated);
+  updateGlobalSolved(globalStats.puzzlesSolved);
+  updateTotalPlayers(globalStats.totalPlayers);
+  setUserScoreboard(userStats.scoreboardPosition);
 }
 
 updateScoreboard();
-updateUserGenerated(0);
-updateUserSolved(0);
-setUserScoreboard(3);
 
+// update loop
 while (true) {
-  let n = Math.floor(Math.random() * 20);
-  updateGlobalGenerated(n);
-  n = Math.floor(Math.random() * 10);
-  updateGlobalSolved(n);
-  n = Math.floor(Math.random() * 3);
-  updateTotalPlayers(n);
+  await update();
   await sleep(5000);
 }
